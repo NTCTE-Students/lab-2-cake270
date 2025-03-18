646f33695685ddabb22329bf1ab5fac6166453fb
cake-270 <kalashnikova.milena@nttek.ru>
2025-03-18 11:39:06 +0300

    ve

--- number_list.php
+++ number_list.php
@@ -1,21 +1,9 @@
-<!DOCTYPE html>
-<html lang="ru">
-<head>
-    <title>Список чисел</title>
-</head>
-<body>
-    <h1>Список чисел от 1 до 10</h1>
-    <ul>
-
-    <?php
+<?php
 
 $number = 1;
 
 while ($number <= 10) {
-    print("<li>$number</li>");
+    print("<li>{$number}</li>");
     $number++;
 }
 ?>
-    </ul>
-</body>
-</html>
--- number_list_do_while.php
+++ number_list_do_while.php
@@ -1,21 +1,9 @@
-<!DOCTYPE html>
-<html lang="ru">
-<head>
-    <title>Список чисел</title>
-</head>
-<body>
-    <h1>Список чисел от 1 до 10</h1>
-    <ul>
-        
-    <?php
+<?php
 
 $number = 1;
 
 do {
-    print("<li>$number</li>");
+    print("<li>{$number}</li>");
     $number++;
 } while ($number <= 10);
 ?>
-    </ul>
-</body>
-</html>
